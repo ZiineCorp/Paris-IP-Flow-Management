52176186119671830e415d6ed0dcc05edcb50c7f
Louis Lavens <104149529+LouisSkyline@users.noreply.github.com>
2022-06-10 14:16:56 +0200

    Add Unit Test (#6)

---
+++ Scripts.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scripts.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communicatons NV")]
+[assembly: AssemblyProduct("Scripts.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communicatons NV 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("991bddc4-0ece-4c1c-9733-fbf0c2a8706e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Scripts.UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Skyline.DataMiner.Automation;
+
+namespace Scripts.UnitTests
+{
+    [TestClass]
+    public class SignalProblemDetectedTests
+    {
+        private Script _script;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _script = new Script();
+        }
+
+        [TestMethod]
+        public void Test_TriggerMainSite1()
+        {
+            var mainElementMock = new Mock<ScriptDummy>();
+            mainElementMock.SetupGet(s => s.ElementName).Returns("PARIS_OFFICE_SITE1_MAIN");
+
+            var backupElement = new Mock<Element>();
+            backupElement.Setup(s => s.GetParameter("LNB1 Supply")).Returns<string>((name) => "1");
+
+            var switchMock = new Mock<Element>();
+
+            var engineMock = new Mock<IEngine>();
+            engineMock.Setup(e => e.GetDummy("IRD")).Returns<string>((name) => mainElementMock.Object);
+            engineMock.Setup(e => e.FindElement("IRD_EC_SITE1_BACKUP")).Returns<string>((name) => backupElement.Object);
+            engineMock.Setup(e => e.FindElement("Site Switch")).Returns<string>((name) => switchMock.Object);
+
+            _script.Run(engineMock.Object);
+
+            switchMock.Verify(s => s.SetParameter("Switch Site 1", 2), Times.Once);
+        }
+    }
+}
